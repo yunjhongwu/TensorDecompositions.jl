a16c0bb0f68e9f5b0fd53546d71a0ff18584252b
Yun-Jhong Wu <yjwu@umich.edu>
2015-08-01 21:27:57 -0700

    Correct tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,9 @@ srand(1)
 T = reshape([1:60], 3,4,5) 
 T_norm = vecnorm(T)
 S = abs(randn(10, 15, 20))
-S_norm = vecnorm(T)
+S_norm = vecnorm(S)
+#X = randn(50, 60, 70)
+#X_norm = vecnorm(X)
 
 println("HOSVD, Case 1")
 @test @time hosvd(T, 2).residual < 1e-7 * T_norm 
@@ -14,11 +16,13 @@ println("CANDECOMP, Case 1")
 println("Non-negative CP, Case 1")
 @test @time ntfcp(T, 2).residual < 0.06 * T_norm
 println("HOSVD, Case 2")
-@test @time hosvd(S, 2).residual < 0.15 * T_norm 
+@test @time hosvd(S, 2).residual < 0.6 * S_norm 
 println("CANDECOMP, Case 2")
-@test @time candecomp(S, 2).residual < 0.15 * T_norm
+@test @time candecomp(S, 2).residual < 0.6 * S_norm
 println("Non-negative CP, Case 2")
-@test @time ntfcp(S, 2).residual < 0.15 * T_norm
+@test @time ntfcp(S, 2).residual < 0.6 * S_norm
+#println("Tensor-CUR")
+#println(tensorcur(X, 10, 30).residual / X_norm)
 
 
 println("All tests passed.")
