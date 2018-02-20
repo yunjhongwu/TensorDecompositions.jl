0f28a7bdc685d58cd7cb96ba4dddcae7bd94ab03
monty <velimir.vesselinov@gmail.com>
2018-02-20 10:02:18 -0700

    dimensionality selection added

--- src/hosvd.jl
+++ src/hosvd.jl
@@ -1,25 +1,34 @@
+import TensorToolbox
+
 """
 High-order singular value decomposition (HO-SVD).
 """
-function hosvd{T,N}(tensor::StridedArray{T,N}, core_dims::NTuple{N, Int};
-                    pad_zeros::Bool=false, compute_error::Bool=false)
-    pad_zeros || _check_tensor(tensor, core_dims)
+function hosvd{T,N}(tensor::StridedArray{T,N}, core_dims::NTuple{N, Int}, dim=trues(N);
+					pad_zeros::Bool=false, compute_error::Bool=false)
+	pad_zeros || _check_tensor(tensor, core_dims)
 
-    factors = map(1:N) do i
-        X = _col_unfold(tensor, i)
-        f = eigs(X'X, nev=core_dims[i])[2]
-        if pad_zeros && size(f, 2) < core_dims[i] # fill missing factors with zeros
-            f = hcat(f, zeros(T, size(tensor, i), core_dims[i]-size(f, 2)))
-        end
-        mapslices(_check_sign, f, 1)
-    end
+	factors = map(1:N) do i
+		X = _col_unfold(tensor, i)
+		if dim[i]
+			f = eigs(X'X, nev=core_dims[i])[2]
+		else
+			f = rand(size(X'))
+		end
+		if pad_zeros && size(f, 2) < core_dims[i] # fill missing factors with zeros
+			warn("Zeros added in dimension $i $(core_dims[i]-size(f, 2))")
+			f = hcat(f, zeros(T, size(tensor, i), core_dims[i]-size(f, 2)))
+		end
+		mapslices(_check_sign, f, 1)
+	end
 
-    res = Tucker((factors...), tensorcontractmatrices(tensor, factors))
-    if compute_error
-        _set_rel_residue(res, tensor)
-    end
-    return res
+	res = Tucker((factors...), tensorcontractmatrices(tensor, factors))
+	if compute_error
+		_set_rel_residue(res, tensor)
+		info("Error: $(res.props[:rel_residue])")
+	end
+	info("HOSVD core rank: $(TensorToolbox.mrank(res.core))")
+	return res
 end
 
 hosvd{T,N}(tensor::StridedArray{T,N}, r::Int; compute_error::Bool=false) =
-    hosvd(tensor, (fill(r, N)...); compute_error=compute_error)
+	hosvd(tensor, (fill(r, N)...); compute_error=compute_error)
